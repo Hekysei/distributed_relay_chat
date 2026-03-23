ea652b307a9da7b06449f19e8113792fa95e822a
Hekysei <150318238+Hekysei@users.noreply.github.com>
2026-03-23 23:49:23 +0800

    Added cli prototype

---
+++ client.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+import curses
+from typing import Union
+
+
+class APP:
+    def __init__(self):
+        self.is_running = False
+        self.messages = ["welcome"]
+        self.input_buffer = ""
+
+        self.stdscr: curses.window
+        self.msg_win: curses.window
+        self.input_win: curses.window
+
+    def run(self, stdscr: curses.window):
+        self.stdscr = stdscr
+        curses.curs_set(1)
+        curses.use_default_colors()
+
+        self.fresah_draw()
+
+        self.is_running = True
+        while self.is_running:
+            self.iter()
+
+    def iter(self):
+        # int - специальные ключи, chr - символ
+        c: Union[int, str] = self.stdscr.get_wch()
+
+        if isinstance(c, int):
+            if c == curses.KEY_RESIZE:
+                self.fresah_draw()
+            elif c in (curses.KEY_BACKSPACE, 127):
+                self.backspace()
+            elif c == 27:
+                # ESC
+                self.is_running = False
+        else:
+            if c in ("\n", "\r") or c == curses.KEY_ENTER:
+                # Enter
+                self.enter()
+            elif c in ("\x7f", "\b"):
+                # Иногда может прилетать такой запрос стереть символ
+                self.backspace()
+            elif c.isprintable() or c.isalpha():
+                # Если символ можно напечатать или он есть в алфавите
+                self.input_buffer += c
+                self.update_input()
+
+    def fresah_draw(self):
+        self.stdscr.clear()
+        self.stdscr.border()
+        self.stdscr.refresh()
+
+        self.resize_windows()
+        self.update_messages()
+        self.update_input()
+
+    def backspace(self):
+        if self.input_buffer:
+            self.input_buffer = self.input_buffer[:-1]
+            self.update_input()
+
+    def enter(self):
+        if message := self.input_buffer.strip():
+            self.add_message(message)
+            self.input_buffer = ""
+            self.update_input()
+
+    def add_message(self, message: str):
+        self.messages.append(message)
+        self.update_messages()
+
+    def resize_windows(self):
+        height, width = self.stdscr.getmaxyx()
+        msg_height = max(1, height - 2)
+        input_height = 1
+
+        self.msg_win = curses.newwin(msg_height, width, 0, 0)
+        self.input_win = curses.newwin(input_height, width, msg_height + 1, 0)
+        self.msg_win.scrollok(True)
+        self.input_win.scrollok(True)
+
+    def update_messages(self):
+        self.msg_win.clear()
+
+        height, width = self.msg_win.getmaxyx()
+        start_idx = max(0, len(self.messages) - height)
+        shift = max(0, height - len(self.messages))
+
+        for i, msg in enumerate(self.messages[start_idx:]):
+            self.msg_win.addstr(shift + i, 0, msg[: width - 1])
+        self.msg_win.refresh()
+
+    def update_input(self):
+        self.input_win.clear()
+        _, width = self.input_win.getmaxyx()
+        self.input_win.addstr(0, 0, self.input_buffer[: width - 1])
+        self.input_win.refresh()
+
+
+if __name__ == "__main__":
+    curses.wrapper(APP().run)
