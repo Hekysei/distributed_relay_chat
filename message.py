ca21eba96fd0666e055baf19d2b41d7dc6ec56a2
Hekysei <150318238+Hekysei@users.noreply.github.com>
2026-03-25 17:30:47 +0800

    Added message class & methods to json<->message

---
+++ message.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass, asdict
+from datetime import datetime
+import json
+
+
+@dataclass
+class Message:
+    room: str
+    autore: str
+    text: str
+    timestamp: datetime | None = None
+
+    def __post_init__(self):
+        if self.timestamp is None:
+            self.timestamp = datetime.now()
+
+
+def json_to_message(json_str: str) -> Message:
+    data = json.loads(json_str)
+    data["timestamp"] = datetime.fromisoformat(data["timestamp"])
+    return Message(**data)
+
+
+def message_to_json(msg: Message) -> str:
+    data = asdict(msg)
+    data["timestamp"] = data["timestamp"].isoformat()
+    return json.dumps(data)
