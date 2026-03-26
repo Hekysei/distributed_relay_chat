8d5cfdfe9685743538138ef5125c8c255de9a74a
Hekysei <150318238+Hekysei@users.noreply.github.com>
2026-03-26 15:25:48 +0800

    Added classes Server & Relay

--- server.py
+++ server.py
@@ -2,76 +2,91 @@
 
 import asyncio
 import websockets
-import json
 import signal
-import sys
 
 from message import Message, json_to_message, message_to_json
 
-connected_clients = set()
+from typing import Callable
 
 
 class ClientHandler:
-    def __init__(self, websocket: websockets.ServerConnection):
-        self.ws = websocket
+    def __init__(self, send_message: Callable[[Message]], relay_call):
+        self.relay_chat_name = "r/relay"
+        self.send_message = send_message
+        self.relay_call = relay_call
+
+    async def on_start(self):
+        await self.send_direct_text("Welcome to relay")
+
+    async def on_msg(self, msg: Message):
+        if msg.chat == self.relay_chat_name:
+            await self.send_direct_text("Got: " + msg.text)
+        else:
+            await self.relay_call(msg)
+
+    async def send_direct_text(self, text: str):
+        await self.send_message(Message(self.relay_chat_name, "relay", text))
+
+
+class Relay:
+    def __init__(self):
+        pass
+
+    def on_message(self, msg: Message):
+        pass
+
+
+class Server:
+    def __init__(self):
+        self.relay = Relay()
+        self.active_connections: set[websockets.ServerConnection] = set()
+
+    async def handler_factory(self, ws: websockets.ServerConnection):
+        self.active_connections.add(ws)
+
+        async def send_message(msg: Message):
+            await ws.send(message_to_json(msg))
+
+        handler = ClientHandler(send_message, self.relay.on_message)
+        await handler.on_start()
+
+        try:
+            async for data in ws:
+                data: str | bytes
+                await handler.on_msg(json_to_message(data))
+        except Exception as e:
+            print(e)
+
+        await ws.close()
+        self.active_connections.remove(ws)
+        print("Client disconnected")
 
     async def run(self):
-        connected_clients.add(self)
-        try:
-            await self.send_direct_message("Welcome to relay")
-            async for data in self.ws:
-                await self.handle_recv(data)
-        finally:
-            print("Client disconnected")
-            await self.close()
+        async with websockets.serve(self.handler_factory, "localhost", 1409):
+            print("Server started. Press Ctrl+C to stop.")
 
-    async def handle_recv(self, data: str | bytes):
-        msg = json_to_message(data)
-        print(msg)
-        await self.send_direct_message(msg.text)
+            loop = asyncio.get_running_loop()
+            stop_future = loop.create_future()
 
-    async def send_direct_message(self, text: str):
-        await self.ws.send(message_to_json(Message("c/relay", "relay", text)))
+            try:
+                loop.add_signal_handler(signal.SIGINT, stop_future.set_result, None)
+                loop.add_signal_handler(signal.SIGTERM, stop_future.set_result, None)
+            except Exception as e:
+                print("Can't set signal")
+                print(e)
+                return
 
-    async def close(self):
-        connected_clients.remove(self)
-        await self.ws.close()
+            await stop_future
 
+            await self.close_active_connections()
 
-async def echo_handler(websocket: websockets.ServerConnection):
-    print("Client connected")
-    response = {"status": "hii"}
-    await websocket.send(json.dumps(response))
-
-
-async def connection_factory(websocket):
-    handler = ClientHandler(websocket)
-    await handler.run()
-
-
-async def main():
-    async with websockets.serve(connection_factory, "localhost", 1409):
-        print("Server started. Press Ctrl+C to stop.")
-
-        loop = asyncio.get_running_loop()
-        stop_future = loop.create_future()
-
-        try:
-            loop.add_signal_handler(signal.SIGINT, stop_future.set_result, None)
-            loop.add_signal_handler(signal.SIGTERM, stop_future.set_result, None)
-        except Exception as e:
-            print("Can't set signal")
-            print(e)
-            return
-
-        await stop_future
-
-        print("Closing all client connections...")
-        # await asyncio.gather(
-        #     *[client.close() for client in connected_clients], return_exceptions=True
-        # )
-        sys.exit(0)
+    async def close_active_connections(self):
+        if self.active_connections:
+            print(f"Closing {len(self.active_connections)} active connections...")
+            close_tasks = [ws.close() for ws in self.active_connections]
+            await asyncio.gather(*close_tasks, return_exceptions=True)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    relay = Server()
+    asyncio.run(relay.run())
