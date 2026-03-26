55f27a1e8124506fffb3e46d5c6eefd9e0cf5570
Hekysei <150318238+Hekysei@users.noreply.github.com>
2026-03-26 16:06:28 +0800

    Splited relay and server. Now relay contains server

---
+++ relay.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import asyncio
+import websockets
+
+from typing import Callable
+
+from message import Message, json_to_message, message_to_json
+from server import Server
+
+
+class ConnectionHandler:
+    def __init__(self, ws: websockets.ServerConnection):
+        self.ws = ws
+
+    async def recv_loop(self):
+        try:
+            async for data in self.ws:
+                data: str | bytes
+                yield json_to_message(data)
+        except Exception as e:
+            print(e)
+
+    async def send_message(self, msg: Message):
+        await self.ws.send(message_to_json(msg))
+
+
+class ClientHandler:
+    def __init__(self, relay_call: Callable[[Message]]):
+        self.connection_handler: ConnectionHandler
+        self.relay_chat_name = "r/relay"
+
+        self.relay_call = relay_call
+
+    async def run(self, ws: websockets.ServerConnection):
+        self.connection_handler = ConnectionHandler(ws)
+
+        await self.on_start()
+        async for msg in self.connection_handler.recv_loop():
+            await self.on_msg(msg)
+
+    async def on_start(self):
+        await self.send_direct_text("Welcome to relay")
+
+    async def on_msg(self, msg: Message):
+        if msg.chat == self.relay_chat_name:
+            await self.send_direct_text("Got: " + msg.text)
+        else:
+            await self.relay_call(msg)
+
+    async def send_direct_text(self, text: str):
+        await self.connection_handler.send_message(
+            Message(self.relay_chat_name, "relay", text)
+        )
+
+
+class Relay:
+    def __init__(self):
+        self.server = Server()
+        self.server.on_connection_callback = self.start_handler
+
+    async def run(self):
+        await self.server.run()
+
+    def on_message(self, msg: Message):
+        print(msg)
+
+    async def start_handler(self, ws: websockets.ServerConnection):
+        client_handler = ClientHandler(self.on_message)
+        await client_handler.run(ws)
+
+
+if __name__ == "__main__":
+    relay = Relay()
+    asyncio.run(relay.run())
--- server.py
+++ server.py
@@ -1,61 +1,20 @@
-#!/usr/bin/env python3
-
 import asyncio
 import websockets
 import signal
 
-from message import Message, json_to_message, message_to_json
-
 from typing import Callable
-
-
-class ClientHandler:
-    def __init__(self, send_message: Callable[[Message]], relay_call):
-        self.relay_chat_name = "r/relay"
-        self.send_message = send_message
-        self.relay_call = relay_call
-
-    async def on_start(self):
-        await self.send_direct_text("Welcome to relay")
-
-    async def on_msg(self, msg: Message):
-        if msg.chat == self.relay_chat_name:
-            await self.send_direct_text("Got: " + msg.text)
-        else:
-            await self.relay_call(msg)
-
-    async def send_direct_text(self, text: str):
-        await self.send_message(Message(self.relay_chat_name, "relay", text))
-
-
-class Relay:
-    def __init__(self):
-        pass
-
-    def on_message(self, msg: Message):
-        pass
 
 
 class Server:
     def __init__(self):
-        self.relay = Relay()
         self.active_connections: set[websockets.ServerConnection] = set()
+
+        self.on_connection_callback: Callable[[websockets.ServerConnection]]
 
     async def handler_factory(self, ws: websockets.ServerConnection):
         self.active_connections.add(ws)
 
-        async def send_message(msg: Message):
-            await ws.send(message_to_json(msg))
-
-        handler = ClientHandler(send_message, self.relay.on_message)
-        await handler.on_start()
-
-        try:
-            async for data in ws:
-                data: str | bytes
-                await handler.on_msg(json_to_message(data))
-        except Exception as e:
-            print(e)
+        await self.on_connection_callback(ws)
 
         await ws.close()
         self.active_connections.remove(ws)
@@ -85,8 +44,3 @@ class Server:
             print(f"Closing {len(self.active_connections)} active connections...")
             close_tasks = [ws.close() for ws in self.active_connections]
             await asyncio.gather(*close_tasks, return_exceptions=True)
-
-
-if __name__ == "__main__":
-    relay = Server()
-    asyncio.run(relay.run())
