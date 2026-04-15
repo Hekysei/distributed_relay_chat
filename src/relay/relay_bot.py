faad22c468216ccbab98c4c7006720fb3c36fae4
Hekysei <150318238+Hekysei@users.noreply.github.com>
2026-04-15 19:49:12 +0800

    Added codes

--- src/relay/relay_bot.py
+++ src/relay/relay_bot.py
@@ -1,5 +1,5 @@
 from src.bot.bot import Bot
-from src.relay.dispatcher.dispatcher import Dispatcher
+from src.relay.dispatcher.dispatcher import DispatchCode, Dispatcher
 
 RELAY_CHAT_NAME = "r/relay"
 RELAY_BOT_NAME = "relay"
@@ -7,23 +7,57 @@ RELAY_BOT_NAME = "relay"
 
 class RelayBot(Bot):
     def __init__(self, dispatcher: Dispatcher):
-        super().__init__(RELAY_CHAT_NAME, RELAY_BOT_NAME, lambda *_args, **_kwargs: None)
+        super().__init__(
+            RELAY_CHAT_NAME, RELAY_BOT_NAME, lambda *_args, **_kwargs: None
+        )
         self.dispatcher = dispatcher
 
         async def join_channel(client_handler, name):
             res = await self.dispatcher.subscribe(name, client_handler.username)
-            await self.async_send_text_to(client_handler, res)
+            if res.ok:
+                await self.async_send_text_to(
+                    client_handler, f"You have subscribed to the room: {name}"
+                )
+                return
+            if res.code == DispatchCode.NO_SUCH_CHANNEL:
+                await self.async_send_text_to(
+                    client_handler, f"There is no room with name: {name}"
+                )
+                return
+            await self.async_send_text_to(client_handler, "Unknown or Error")
 
         async def add_channel(client_handler, name):
             res = await self.dispatcher.add_channel(name)
-            await self.async_send_text_to(client_handler, res)
-            await join_channel(client_handler, name)
+            if res.ok:
+                await self.async_send_text_to(
+                    client_handler, f"Room {name} successfully created"
+                )
+                await join_channel(client_handler, name)
+                return
+            if res.code == DispatchCode.CHANNEL_ALREADY_EXISTS:
+                await self.async_send_text_to(
+                    client_handler, f"Room {name} already exists"
+                )
+                return
+            await self.async_send_text_to(client_handler, "Unknown or Error")
 
         async def verify(client_handler):
             res = await self.dispatcher.add_user(
                 client_handler.username, client_handler.send_message
             )
-            await self.async_send_text_to(client_handler, res)
+            if res.ok:
+                await self.async_send_text_to(
+                    client_handler,
+                    f"You are verified. Your name: {client_handler.username}",
+                )
+                return
+            if res.code == DispatchCode.USERNAME_TAKEN:
+                await self.async_send_text_to(
+                    client_handler,
+                    f"The name {client_handler.username} is already taken",
+                )
+                return
+            await self.async_send_text_to(client_handler, "Unknown or Error")
 
         name_kwargs = {
             "name": "blank_name",
